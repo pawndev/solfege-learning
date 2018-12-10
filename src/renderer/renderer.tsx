8499747e068e19c6d9c891921384affc7b2c4546
pawndev <coquelet.c@gmail.com>
2018-12-10 13:16:52 +0100

    Add HashRouter

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react';
 import { Fragment } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './style.css';
 
 import Home from '../pages/home/index';
 import Menu from '../pages/menu/index';
 
 render(
-  <BrowserRouter>
+  <HashRouter>
     <Fragment>
-      {window.location.pathname.includes('index.html') && <Redirect to="/" />}
       <Switch>
         <Route component={Home} exact={true} path="/home"/>
         <Route component={Menu} exact={true} path="/" />
         <Route component={Home} />
       </Switch>
     </Fragment>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('app')
 );
